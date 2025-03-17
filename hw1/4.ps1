08c5549db86d8d94e5f4c20facb00d5464481575
Hayden Chang <m831718@gmail.com>
2025-03-18 01:03:53 +0800

    fix(hw1): correct Image property extraction

--- hw1/4.ps1
+++ hw1/4.ps1
@@ -14,5 +14,5 @@ Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-Sysmon/Operational'; 
 
 # 查詢系統中任何一個「檔案建立」事件 (Event ID: 11)
 Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-Sysmon/Operational'; Id=11} |
-    Select-Object -Property TimeCreated, @{Name='TargetFilename'; Expression={($_.Properties[5].Value)}}, @{Name='Process'; Expression={($_.Properties[3].Value)}}, @{Name='Image'; Expression={($_.Properties[5].Value)}} |
+    Select-Object -Property TimeCreated, @{Name='TargetFilename'; Expression={($_.Properties[5].Value)}}, @{Name='Process'; Expression={($_.Properties[3].Value)}}, @{Name='Image'; Expression={($_.Properties[4].Value)}} |
     Select-Object -First 1 | Format-Table -AutoSize
